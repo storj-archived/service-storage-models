a988ab0bc7ef070483153b742199896713c93598
Barbara Liau <bj.liau@gmail.com>
2016-12-05 14:28:11 -0700

    Passing Storage/models/Credit 'paid_amount' tests

--- test/credit.unit.js
+++ test/credit.unit.js
@@ -13,6 +13,7 @@ const CreditSchema = require('../lib/models/credit');
 const constants = require('../lib/constants');
 const CREDIT_TYPES = constants.CREDIT_TYPES;
 const PAYMENT_PROCESSORS = constants.PAYMENT_PROCESSORS;
+const PROMO_CODES = constants.PROMO_CODES;
 
 var Credit;
 var connection;
@@ -50,45 +51,42 @@ describe('Storage/models/Credit', function() {
         expect(credit.paid_amount).to.equal(0);
         expect(credit.invoiced_amount).to.equal(0);
         expect(credit.user).to.equal('user@domain.tld');
-        expect(credit.promo_code).to.be.null;
+        expect(credit.promo_code).to.equal(PROMO_CODES.DEFAULT);
         expect(credit.promo_amount).to.equal(0);
         expect(credit.paid).to.be.false;
         expect(credit.created).to.equalDate(date);
-        expect(credit.payment_processor).to.be.null;
+        expect(credit.payment_processor).to.equal(PAYMENT_PROCESSORS.DEFAULT);
         expect(credit.type).to.be.oneOf(
             Object.keys(CREDIT_TYPES).map((key) => (CREDIT_TYPES[key]))
         );
         expect(credit.data).to.be.null;
-        done()
-      });
-    });
-
-    it('should create credit with paid equals false', function(done) {
-      var newCredit = new Credit({
-        user: 'user@domain.tld',
-        type: CREDIT_TYPES.MANUAL,
-        invoiced_amount: 100
-      });
-
-      newCredit.save(function(err, credit) {
-        expect(err).to.not.be.instanceOf(Error);
-        expect(credit.paid).to.equal(false);
         done();
       });
     });
 
-    it('should fail with non-currency paid_amount', function(done) {
+    it('should convert non-currency paid_amount to 0', function(done) {
       var newCredit = new Credit({
         user: 'user@domain.tld',
         type: CREDIT_TYPES.MANUAL,
         invoiced_amount: 100,
         paid_amount: 'text'
-        // only fails with null
-        // passing in undefined or '' makes default: 0
       });
 
       newCredit.save(function(err, credit) {
-        console.log('credit', credit);
+        expect(err).to.not.be.instanceOf(Error);
+        expect(credit.paid_amount).to.equal(0);
+        done();
+      });
+    });
+
+    it('should fail when paid_amount equals null', function(done) {
+      var newCredit = new Credit({
+        user: 'user@domain.tld',
+        type: CREDIT_TYPES.MANUAL,
+        paid_amount: null
+      });
+
+      newCredit.save(function(err, credit) {
         expect(err).to.be.instanceOf(Error);
         done();
       });
@@ -100,7 +98,7 @@ describe('Storage/models/Credit', function() {
 
 /*
  fail with non-currency paid_amount
- any payment processor has to match enums
+
  promo code has to be String type
  user always needs to be email (regex)
  paid amount should never be greater than invoiced_amount
