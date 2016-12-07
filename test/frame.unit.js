2422dcc609416476eafa0a431f436b640967157a
Barbara Liau <bj.liau@gmail.com>
2016-12-07 15:46:53 -0700

    Passing tests checking create, lock, and unlock methods

--- test/frame.unit.js
+++ test/frame.unit.js
@@ -3,12 +3,15 @@
 // const storj = require('storj-lib');
 const expect = require('chai').expect;
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 require('mongoose-types').loadTypes(mongoose);
 
 const FrameSchema = require('../lib/models/frame');
+const UserSchema = require('../lib/models/user');
 
 var Frame;
+var User;
 var connection;
 
 before(function(done) {
@@ -16,6 +19,7 @@ before(function(done) {
     'mongodb://127.0.0.1:27017/__storj-bridge-test',
     function() {
       Frame = FrameSchema(connection);
+      User = UserSchema(connection);
       done();
     }
   );
@@ -23,12 +27,61 @@ before(function(done) {
 
 after(function(done) {
   Frame.remove({}, function() {
-    connection.close(done);
+    User.remove({}, function() {
+      connection.close(done);
+    });
   });
 });
 
+function sha256(i) {
+  return crypto.createHash('sha256').update(i).digest('hex');
+}
+
 describe('Storage/models/Frame', function() {
+
   describe('#create', function() {
 
-  })
+    it('should create a frame with default props', function(done) {
+      User.create('user@domain.tld', sha256('password'), function(err, user) {
+        Frame.create(user, function(err, frame) {
+          expect(err).to.not.be.an.instanceOf(Error);
+          expect(frame.created).to.be.an.instanceOf(Date);
+          expect(frame.user).to.be.a('string');
+          expect(frame.locked).to.be.false;
+          expect(frame.size).to.equal(0);
+          expect(frame.shards).to.be.an('array');
+
+          // cleanup
+          User.findOneAndRemove({ _id: user._id }, function(err, user) {
+            done();
+          });
+        });
+      });
+    });
+
+    it('should set frame.lock to true', function(done) {
+      Frame.create({}, function(err, frame) {
+        expect(frame.locked).to.be.false;
+        frame.lock(function(err, frame) {
+          expect(frame.locked).to.be.true;
+          done();
+        });
+      });
+    });
+
+    it('should set frame.lock to false', function(done) {
+      Frame.create({}, function(err, frame) {
+        expect(frame.locked).to.be.false;
+        frame.lock(function(err, frame) {
+          expect(frame.locked).to.be.true;
+          frame.unlock(function(err, frame) {
+            expect(frame.locked).to.be.false;
+            done();
+          });
+        });
+      });
+    });
+
+  });
+
 });
