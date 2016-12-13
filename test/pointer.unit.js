cf02c66e14c4638361f52aa3e794a11b8660f396
Barbara Liau <bj.liau@gmail.com>
2016-12-12 22:20:34 -0700

    Add test for create method

--- test/pointer.unit.js
+++ test/pointer.unit.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// const storj = require('storj-lib');
 const expect = require('chai').expect;
 const mongoose = require('mongoose');
 
@@ -28,7 +27,31 @@ after(function(done) {
 });
 
 describe('Storage/models/Pointer', function() {
+
   describe('#create', function() {
 
-  })
+    it('should create pointer with correct props', function(done) {
+      var shard = {
+        index: 1,
+        hash: 'fjla93fs9-23892-2sdl@#ds-932049203',
+        size: 1,
+        tree: ['tree1', 'tree2'],
+        challenges: ['challenge1', 'challenge2']
+      };
+      Pointer.create(shard, function(err, pointer) {
+        expect(err).to.not.be.an.instanceOf(Error);
+        expect(pointer.index).to.equal(shard.index);
+        expect(pointer.index).to.be.a('number');
+        expect(pointer.hash).to.equal(shard.hash);
+        expect(pointer.hash).to.be.a('string');
+        expect(pointer.size).to.equal(shard.size);
+        expect(pointer.size).to.be.a('number');
+        expect(pointer.tree).to.be.an('array');
+        expect(pointer.challenges).to.be.an('array');
+        done();
+      });
+    });
+
+  });
+
 });
