06933730b9237147fbe2d89887e85b324def047a
Barbara Liau <bj.liau@gmail.com>
2016-12-12 21:45:22 -0700

    Shard.create tests

--- test/shard.unit.js
+++ test/shard.unit.js
@@ -28,7 +28,105 @@ after(function(done) {
 });
 
 describe('Storage/models/Shard', function() {
+
   describe('#create', function() {
 
-  })
+    it('should create a shard record from the supplied item', function(done) {
+      var item = {
+        hash: 'fjla93fs9-23892-2sdl@#ds-932049203',
+        contracts: [],
+        trees: [],
+        challenges: [],
+        meta: []
+      };
+      Shard.create(item, function(err, shard) {
+        expect(err).to.not.be.an.instanceOf(Error);
+        expect(shard.hash).to.equal(item.hash);
+        expect(shard.meta).to.be.an('array');
+        expect(shard.challenges).to.be.an('array');
+        expect(shard.trees).to.be.an('array');
+        expect(shard.contracts).to.be.an('array');
+        done();
+      });
+    });
+
+    it('should create a shard record with modified contracts array', function(done) {
+      var item = {
+        hash: 'fjla93fs9-23892-2sdl@#ds-932049203',
+        contracts: [{ type: 'contract1' }, { type: 'contract2' }],
+        trees: [],
+        challenges: [],
+        meta: []
+      };
+      Shard.create(item, function(err, shard) {
+        expect(err).to.not.be.an.instanceOf(Error);
+        expect(shard.contracts).to.be.an('array');
+        shard.contracts.forEach((contract, index) => {
+          expect(contract.nodeID).to.equal(index.toString());
+          expect(contract.contract).to.equal(item.contracts[index]);
+        });
+        done();
+      });
+    });
+
+    it('should create a shard record with modified trees array', function(done) {
+      var item = {
+        hash: 'fjla93fs9-23892-2sdl@#ds-932049203',
+        contracts: [],
+        trees: ['tree1', 'tree2', 'tree3'],
+        challenges: [],
+        meta: []
+      };
+      Shard.create(item, function(err, shard) {
+        expect(err).to.not.be.an.instanceOf(Error);
+        expect(shard.trees).to.be.an('array');
+        shard.trees.forEach((tree, index) => {
+          expect(tree.nodeID).to.equal(index.toString());
+          expect(tree.tree).to.be.an('array');
+          expect(tree.tree[0]).to.equal(item.trees[index])
+        });
+        done();
+      });
+    });
+
+    it('should create a shard record with modified challenges array', function(done) {
+      var item = {
+        hash: 'fjla93fs9-23892-2sdl@#ds-932049203',
+        contracts: [],
+        trees: [],
+        challenges: [{ type: 'challenge1' }, { type: 'challenge2' }],
+        meta: []
+      };
+      Shard.create(item, function(err, shard) {
+        expect(err).to.not.be.an.instanceOf(Error);
+        expect(shard.challenges).to.be.an('array');
+        shard.challenges.forEach((challenge, index) => {
+          expect(challenge.nodeID).to.equal(index.toString());
+          expect(challenge.challenge).to.equal(item.challenges[index]);
+        });
+        done();
+      });
+    });
+
+    it('should create a shard record with modified meta array', function(done) {
+      var item = {
+        hash: 'fjla93fs9-23892-2sdl@#ds-932049203',
+        contracts: [],
+        trees: [],
+        challenges: [],
+        meta: [{ type: 'meta1' }, { type: 'meta2' }]
+      };
+      Shard.create(item, function(err, shard) {
+        expect(err).to.not.be.an.instanceOf(Error);
+        expect(shard.meta).to.be.an('array');
+        shard.meta.forEach((metaItem, index) => {
+          expect(metaItem.nodeID).to.equal(index.toString());
+          expect(metaItem.meta).to.equal(item.meta[index]);
+        });
+        done();
+      });
+    });
+
+  });
+
 });
