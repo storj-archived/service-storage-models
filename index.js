4a4c363361793318260d037de9b6d62f0236bb8f
Barbara Liau <bj.liau@gmail.com>
2017-02-03 22:52:07 -0700

    Remove unused dependencies

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const merge = require('merge');
 require('dotenv').config({ silent: true });
 
-require('mongoose-currency').loadType(mongoose);
 require('mongoose-types').loadTypes(mongoose);
 
 mongoose.Promise = global.Promise;
