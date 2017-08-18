d7b797ed0ac291af7213ebf22f46a7664d920f88
Braydon Fuller <braydon@storj.io>
2017-08-17 20:47:59 -0400

    Migration fixes

--- migration/mirrors-ttl.js
+++ migration/mirrors-ttl.js
@@ -1,28 +1,26 @@
 var addCreatedFromContractStart = function() {
   var cursor = db.mirrors.find({created: { $exists: false }});
 
-  function updateNextDocument() {
-    if (cursor.hasNext()) {
+  while(cursor.hasNext()) {
 
-      var doc = cursor.next();
-      var id = doc._id;
-      var created = doc.contract.store_begin;
-      print("updating id: " + id + " with created: " + created);
-      db.mirrors.update({
-        _id: id
-      }, {
-        $set: {
-          created: created
-        }
-      }, (err) => {
-        if (err) {
-          print(err);
-        }
-        updateNextDocument();
-      });
-      
+    var doc = cursor.next();
+    var id = doc._id;
+    var created = doc.contract.store_begin;
+    print("updating id: " + id + " with created: " + created);
+    var result = db.mirrors.update({
+      _id: id
+    }, {
+      $set: {
+        created: created
+      }
+    });
+
+    if (result.nModified) {
+      print("updated id: " + id);
     }
+
   }
+
 };
 
 addCreatedFromContractStart();
